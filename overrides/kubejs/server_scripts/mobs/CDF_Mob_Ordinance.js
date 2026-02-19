d14b259899c3096616aaad25a20f9a13fa594a39
recon <kalanicook@yahoo.com>
2026-02-19 10:23:41 -1000

    remove: lost lore mod causing crashes

--- overrides/kubejs/server_scripts/mobs/CDF_Mob_Ordinance.js
+++ overrides/kubejs/server_scripts/mobs/CDF_Mob_Ordinance.js
@@ -9,9 +9,10 @@ BlockEvents.placed(event => {
   if (level.isClientSide && level.isClientSide()) return
   if (level.dimension != 'minecraft:the_nether') return
   if (block.id == 'minecraft:spawner')
-  if (block.y <= NETHER_ROOF_Y) return
- 
-  event.cancel()
+  if (block.id == 'minecraft:spawner') {
+    if (block.y <= NETHER_ROOF_Y) return
+    event.cancel()
+  }
 })
 
 BlockEvents.rightClicked(event => {
